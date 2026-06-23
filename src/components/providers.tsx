5409ca4b96aae8598afd09cdbb33c4a73ac5d8bf
zironpro <dev@zironpro.ae>
2026-06-23 15:27:52 +0530

    feat: implement root layout and shared providers with analytics and routing progress support

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,18 +3,22 @@
 import React from "react";
 
 import { ProgressProvider } from "@bprogress/next/app";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<ProgressProvider
-			color="#D40C1A"
-			height="3px"
-			memo
-			options={{ showSpinner: false }}
-			shallowRouting
-		>
-			{children}
-		</ProgressProvider>
+		<>
+			<ProgressProvider
+				color="#D40C1A"
+				height="3px"
+				memo
+				options={{ showSpinner: false }}
+				shallowRouting
+			>
+				{children}
+			</ProgressProvider>
+			<GoogleAnalytics gaId="G-N3D28GZSBM" />
+		</>
 	);
 };
 
